b3263184fdab50543172092d7f1c83c3a97765b6
Dheeraj S <127111486+dheeraj-codingdesk@users.noreply.github.com>
2025-11-16 01:50:12 +0530

    Update Firebase configuration with placeholder values

--- Backend/services/supabase.ts
+++ Backend/services/supabase.ts
@@ -3,14 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD_f9SwUswhUK36ig_D9L26vQFTv_iCp5s",
-  authDomain: "agrox-aae2a.firebaseapp.com",
-  databaseURL: "https://agrox-aae2a-default-rtdb.firebaseio.com",
+  apiKey: "API",
+  authDomain: "auth domain",
+  databaseURL: "database url",
   projectId: "agrox-aae2a",
   storageBucket: "agrox-aae2a.firebasestorage.app",
   messagingSenderId: "266693709737",
-  appId: "1:266693709737:web:8e83f002ef6621ad8d3eb8",
-  measurementId: "G-T23JCN5CVE"
+  appId: "appid",
+  measurementId: "G-T23JCN5CVE  "
 };
 
 const app = initializeApp(firebaseConfig);
